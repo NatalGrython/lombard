2587ce1e754fdc58d0d35fa5089213c2d2547b6b
NatalGrypthon <altair.al-jabavi@yandex.ru>
2021-10-10 01:11:57 +0300

    Add catalog

---
+++ client/src/components/Catalog/index.module.scss.d.ts
@@ -0,0 +1,17 @@
+declare const styles: {
+  readonly "button": string;
+  readonly "catalog": string;
+  readonly "catalog__wrapper": string;
+  readonly "category": string;
+  readonly "image": string;
+  readonly "info__wrapper": string;
+  readonly "name": string;
+  readonly "name__wrapper": string;
+  readonly "price": string;
+  readonly "product": string;
+  readonly "span": string;
+  readonly "title": string;
+  readonly "title__wrapper": string;
+};
+export = styles;
+
